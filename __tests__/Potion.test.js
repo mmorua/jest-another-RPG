e6b17fca057b8b9ec633d5a87d1f5cd95f8b2821
Marcus Morua <marcus.morua@gmail.com>
2022-01-15 16:40:37 -0600

    feat: Add random Potion object jest test

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,10 +1,20 @@
 // My test function - Should be getting 1 failed, 1 passed - Only getting 1 failed - Rather 1 test in general
 
+const { expect } = require('@jest/globals');
+const { test } = require('picomatch');
 const Potion = require('../lib/Potion.js');
 
 test('creates a health potion object', () => {
     const potion = new Potion('health');
   
     expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('creates a random portion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length)toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
 });
